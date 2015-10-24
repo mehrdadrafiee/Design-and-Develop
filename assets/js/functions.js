4f4a08aadd6647f6f5c6c0b0b120993909e39295
Mehrdad Rafiee <rafiee.mehrdad@gmail.com>
2015-10-23 18:52:01 -0700

    added the youtube section

---
+++ assets/js/functions.js
@@ -0,0 +1,11 @@
+//$(function() {});
+
+$(window).scroll(function () {
+	youtubeVidScroll();
+});
+
+function youtubeVidScroll() {
+	var wScroll = $(window).scrollTop();
+
+	$('.video-strip').css('background-position', 'center -' + wScroll + '0px');
+}
