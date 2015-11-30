a0b1b185fe7d4ef460a03efdee642f297b468ff8
Mehrdad Rafiee <rafiee.mehrdad@gmail.com>
2015-11-29 17:06:39 -0800

    removed positioning of face elects. will add it later

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -5,11 +5,6 @@ $(function() {
 function mentoringBubbleClick() {
 	$('.face').on('click', function() {
 		console.log("face clicked!");
-		var faceTop = $(this).position().top,
-			verMath = faceTop - 135;
-
-		console.log(verMath);
-
 		$(this).addClass('has-bubble-open')
 			.siblings().removeClass('has-bubble-open');
 	});
